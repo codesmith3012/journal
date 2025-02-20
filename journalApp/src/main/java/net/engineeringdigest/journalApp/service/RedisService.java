6f873c9bf8bf374b0acc312b5125aeb179e5588f
Samridh Garg <samridh.garg@castler.com>
2025-02-20 10:25:25 +0530

    commit 2

---
+++ journalApp/src/main/java/net/engineeringdigest/journalApp/service/RedisService.java
@@ -0,0 +1,42 @@
+package net.engineeringdigest.journalApp.service;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
+import net.engineeringdigest.journalApp.api.response.WeatherResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Service;
+
+import java.util.concurrent.TimeUnit;
+
+@Slf4j
+@Service
+public class RedisService {
+
+    @Autowired
+    private RedisTemplate<String, Object> redisTemplate;
+
+    public <T> T get(String key, Class<T> entityClass) {
+        try {
+            Object o = redisTemplate.opsForValue().get(key);
+            if (o == null) {
+                return null;
+            }
+            ObjectMapper mapper = new ObjectMapper();
+            return mapper.readValue(o.toString(), entityClass);
+        } catch (Exception e) {
+            log.error("Exception while fetching data from Redis", e);
+            return null;
+        }
+    }
+
+    public void set(String key, Object o, Long ttl) {
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            String json = mapper.writeValueAsString(o);
+            redisTemplate.opsForValue().set(key, json, ttl, TimeUnit.SECONDS);
+        } catch (Exception e) {
+            log.error("Exception while setting data in Redis", e);
+        }
+    }
+}
